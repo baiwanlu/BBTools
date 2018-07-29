2034b1819503bc2274744213e1f7464332d5dfdd
baiwanlu <870533813@qq.com>
2018-07-29 17:14:06 +0800

    初始化

---
+++ BBTools/Classes/Main/MainVC.m
@@ -0,0 +1,38 @@
+//
+//  MainVC.m
+//  test_app
+//
+//  Created by 小小白 on 2018/7/24.
+//  Copyright © 2018年 小小白. All rights reserved.
+//
+
+#import "MainVC.h"
+
+@interface MainVC ()
+
+@end
+
+@implementation MainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"主页";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
